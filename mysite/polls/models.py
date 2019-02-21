5bdb80ef33e8b93d4ddbe932e48575b9c99e7f22
Kevin Wood <kevin.wood75@gmail.com>
2019-02-20 21:26:49 -0500

    initial

---
+++ mysite/polls/models.py
@@ -0,0 +1,6 @@
+from mongoengine import Document, fields
+
+class Poll(Document):
+    poll_name = fields.StringField(required=True)
+    poll_votes = fields.IntField(required=True)
+    poll_date = fields.StringField(required=True)
